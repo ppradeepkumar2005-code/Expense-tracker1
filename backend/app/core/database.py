caf95b5463647950cd9e9792fa4f536346939df7
Pradeep kumar <ppradeepkumar.2005@gmail.com>
2026-06-08 15:40:24 +0530

    fix get_db missing issue

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,25 +1,26 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# ---------------- DATABASE URL ----------------
-# SQLite (testing)
+# ---------------- DATABASE ----------------
 DATABASE_URL = "sqlite:///./expense.db"
 
-# If PostgreSQL use this:
-# DATABASE_URL = "postgresql://user:password@host:port/dbname"
-
-# ---------------- ENGINE ----------------
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
 
-# ---------------- SESSION ----------------
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
-# ---------------- BASE ----------------
 Base = declarative_base()
+
+# ---------------- DB DEPENDENCY ----------------
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
